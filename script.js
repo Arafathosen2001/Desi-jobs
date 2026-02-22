6b89f68fe330b082c1d7a5122da8476e30d36dc3
Arafathosen2001 <aarafat985@gmail.com>
2026-02-22 11:31:44 +0600

    add toggle button all,interview,rejected || 2026-02-22 11:31 AM

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let rejectedCount = document.getElementById('rejected-count');
 let jobCount = document.getElementById('job-count-value');
 
 
-interview.push({firstName: 'John'},{lastName: 'Doe'});
+// interview.push({firstName: 'John'},{lastName: 'Doe'});
 function updateCounts() { 
     const totalJoba = document.getElementById('all-card').children.length;
     interviewCount.innerText = interview.length;
@@ -16,3 +16,26 @@ function updateCounts() {
     jobCount.innerText = totalJoba;
 }
 updateCounts();
+
+
+function toggle(buttonId) {
+    // console.log('click all',buttonId)
+    const selectBtn = document.getElementById(buttonId);
+    // selectBtn.classList.add('btn-primary');
+    const allBtn = document.getElementById('all-btn');
+    const interviewBtn = document.getElementById('interview-btn');
+    const rejectedBtn = document.getElementById('rejected-btn');
+    if(buttonId === 'all-btn'){
+        allBtn.classList.add('btn-primary');
+        interviewBtn.classList.remove('btn-primary');
+        rejectedBtn.classList.remove('btn-primary');
+    } else if(buttonId === 'interview-btn'){
+        allBtn.classList.remove('btn-primary');
+        interviewBtn.classList.add('btn-primary');
+        rejectedBtn.classList.remove('btn-primary');
+    } else if(buttonId === 'rejected-btn'){
+        allBtn.classList.remove('btn-primary');
+        interviewBtn.classList.remove('btn-primary');
+        rejectedBtn.classList.add('btn-primary');
+    }
+}
